30690ee97edc7e63aa39645e7727985df73fc72d
ghasemisahar425 <115500048+ghasemisahar425@users.noreply.github.com>
2022-11-02 11:01:49 -0700

    practice 4 _ 1

---
+++ P4.1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+ main() { 
+ float  x , y , z , t , g , f ;
+ 
+ cin >> x >> y;
+ z = x * y;
+ cout << "zarb: " << z << " ";
+  t = x / y;
+ cout << "tagsim: "<<t << " ";
+ g = x + y;
+ cout << "jam: " <<g << " ";
+ f = x - y;
+ cout << "tafrigh: " << f << " ";
+ 
+}
