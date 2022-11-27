de0e550d65fe0704966b085a4aa4a9817b004fbd
ghasemisahar425 <115500048+ghasemisahar425@users.noreply.github.com>
2022-11-27 13:20:02 -0800

    practice p7-5
    practice p7-5

--- main.cpp
+++ main.cpp
@@ -4,23 +4,17 @@ using namespace std;
 
 int main(int argc, char** argv) {
 
-int m,n,max,min,r=1;
-cout<<"please enter two positive integer:";
-cout<<"\n"<<"num1=";
-cin>>m;
-cout<<"num2=";
-cin>>n;
-max=m;
-min=n;
-while(r!=0)
-{
-r=(max%min);
-max=min;
-min=r;
-}
-cout<<"\n\n"<<"GCD for "<<m<<'&'<<n<<" is:"<<max;
-
-
-}
-
-
+	int n,x=2,i,k,sum=0;
+	cout<<"enter a num:";
+	cin>>n;
+		do{for (i=2,k=0;i<=x/2&&k==0;i++)
+			if(x%i==0)
+			k++;
+		if(k==0)
+		  cout << x << "  ";
+		 x++;
+		 }
+		while (x<=n);{
+			
+		}
+    	}
