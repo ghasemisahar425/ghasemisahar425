9a67664b5757861e6caf0fa4746e52d380a44a3a
ghasemisahar425 <115500048+ghasemisahar425@users.noreply.github.com>
2022-11-27 13:11:27 -0800

    practice p7-1
    practice p7-1

--- main.cpp
+++ main.cpp
@@ -2,52 +2,18 @@
 
 using namespace std;
 
-int main() {
-	char x;
-   	cout<<"please write the letter:";
-	cin>>x;
-    switch (x){
-    	case 'a':{
-    		x='a';
-		cout<<"is vowel:"<<x<<"  ";
-        	break;
-		}
-    	case 'A':{
-    		x='A';
-		cout<<"is vowel:"<<x<<"  ";
-        	break;
-		}
-    	case 'e':{
-    		x='e';
-		cout<<"is vowel:"<<x<<"  ";
-        	break;
-		}
-    	case 'E':{
-    		x='E';
-		cout<<"is vowel:"<<x<<"  ";
-        	break;
-		}		
-    	case 'i':{
-    		x='i';
-		cout<<"is vowel:"<<x<<"  ";
-        	break;
-		}
-    	case 'I':{
-    		x='I';
-		cout<<"is vowel:"<<x<<"  ";
-        	break;
-		}
-    	case 'o':{
-    		x='o';
-		cout<<"is vowel:"<<x<<"  ";
-        	break;
-		}	
-    	case 'O':{
-    		x='O';
-		cout<<"is vowel:"<<x<<"  ";
-        	break;
-		}		
-
-	    }
-
+int main(int argc, char** argv) {
+	int i , x , n , sum = 1;
+	 cout<< "enter first number :";
+	 cin>> x ;
+	 cout<< "enter seconed number :";
+	 cin>> n ; 
+	 for (i=1 ; i<=n ; i++){
+	    sum*=x;
+		cout << sum << "\n";
+	 }
+	  cout<< " etmem ";
+	
+	
+	
 }
