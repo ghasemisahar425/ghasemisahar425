188242bc2fc3b4802bfefd2f4a61a639b7b69dd4
ghasemisahar425 <115500048+ghasemisahar425@users.noreply.github.com>
2022-11-27 13:18:39 -0800

    practice p7-4
    practice p7-4

--- main.cpp
+++ main.cpp
@@ -3,15 +3,24 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-    int n , i , sum; 
-    cout<<" enter number  :";
-    cin >>n;
-    sum = 0;
-    for(int i = 1; i <= n ; i++){
-        if( n % i == 0 ){
-            sum+=i;
-            cout <<i<<"\n";
-        }
-    }
-    cout <<"majmou :"<<sum;
+
+int m,n,max,min,r=1;
+cout<<"please enter two positive integer:";
+cout<<"\n"<<"num1=";
+cin>>m;
+cout<<"num2=";
+cin>>n;
+max=m;
+min=n;
+while(r!=0)
+{
+r=(max%min);
+max=min;
+min=r;
 }
+cout<<"\n\n"<<"GCD for "<<m<<'&'<<n<<" is:"<<max;
+
+
+}
+
+
