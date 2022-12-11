6f8a5a1c9f257723d664c2d30c19e6c8d5dcfe4f
ghasemisahar425 <115500048+ghasemisahar425@users.noreply.github.com>
2022-12-11 15:05:14 -0800

    practice p9-2
    practice p9-2

--- main.cpp
+++ main.cpp
@@ -1,29 +1,32 @@
 #include <iostream>
-#define s 10
-using namespace std;
+#define x 10
+
+ using namespace std;
 
 int main(int argc, char** argv) {
-int array[s]={1,12,23,44,65,76,97,108,111};
-int i,j,temp;
+int array[x]={99,12,15,18,20,22,24,26,28,30};
+int i,j,w,min=0,temp;
 
-	for (int i = 0; i < s ; i++){
-		cout << "array[" << i << "]="<<array[i]<<endl;
-		
-}
-	while(array[10]){
-		cout<<"please array["<<array[10]<<"]:";
-		cin>>array[10];
-		break;
-}
-	for(i=0;i<s;i++){
-		for(j=0;j<(s-i);j++)
-			if(array[j]>array[j+1]){
-				temp=array[j];
-				array[j]=array[j+1];
-				array[j+1]=temp;
+	for(i=0;i<x;i++){
+		cout<<"array value : "<<array[i]<<"\n";
+//		cin>>array[i];
+	}
+	for(i=0;i<x;i++){
+		for(j=0;j<x-1;j++){
+			if(array[i]<array[j]){
+				temp=array[i];
+				array[i]=array[j];
+				array[j]=temp;
+				cout<<"moratab shode:"<<array[j]<<"\n";
 			}
+		}
 	}
-	for(i=0;i<=s;i++){
-		cout<<"array "<<i<<" number is"<<array[i]<<"\n";
-	}		
-	}
+	min=array[0];
+	for(i=1;i<x;i++){
+			if(array[i]<min){
+				min=array[i];
+			}
+}
+				cout<<"the min number is:"<<min<<"\n";
+				cout<<"the second min number is:"<<array[1];
+}
