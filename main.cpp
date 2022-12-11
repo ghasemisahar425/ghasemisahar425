a18bb85cdae7b0b3d70073b8b7cb14498335c385
ghasemisahar425 <115500048+ghasemisahar425@users.noreply.github.com>
2022-12-11 15:04:22 -0800

    practice p9-1
    practice p9-1

--- main.cpp
+++ main.cpp
@@ -1,67 +1,29 @@
 #include <iostream>
-# define int [i] 4 int[j] 4
-
+#define s 10
 using namespace std;
 
-int main(int argc, char** argv) 
-{
+int main(int argc, char** argv) {
+int array[s]={1,12,23,44,65,76,97,108,111};
+int i,j,temp;
 
-//    const float result[0];
-    int  i,a,b,j,sum[i,num[j]];
-        cout<<"Enter a"<<i<<endl;
-	    for(i=0;i<=4;i++){
-	    	cin>> a;
-	    	
-		}
-        cout<<"Enter b"<<endl;
-        for(j=0;j<=4;j++){
-        cin>>b;	
-		}
-			
-        for (int i = 0; i < 2; ++i){
-        if(sum[i]==num[j]){
-		    for (int j = 0; j < 2; ++j){
-				result[i][j] = sum[i][j] + num[i][j];
-}
-}
-}
-
-		}
-//        for(w=0;w)
-  //      a+=b;
-    //    b=a-b;
-      //  a=a-b;
-       // cout<<"a ="<<a<<endl;
-        //cout<<"b ="<<b<<endl;
+	for (int i = 0; i < s ; i++){
+		cout << "array[" << i << "]="<<array[i]<<endl;
 		
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
+	while(array[10]){
+		cout<<"please array["<<array[10]<<"]:";
+		cin>>array[10];
+		break;
+}
+	for(i=0;i<s;i++){
+		for(j=0;j<(s-i);j++)
+			if(array[j]>array[j+1]){
+				temp=array[j];
+				array[j]=array[j+1];
+				array[j+1]=temp;
+			}
+	}
+	for(i=0;i<=s;i++){
+		cout<<"array "<<i<<" number is"<<array[i]<<"\n";
+	}		
+	}
